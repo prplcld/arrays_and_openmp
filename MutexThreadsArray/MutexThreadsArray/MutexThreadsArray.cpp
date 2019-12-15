24c531c4aa439c8864e8dd10791e44847056aa35
AntonSilebin <43464727+AntonSilebin@users.noreply.github.com>
2019-12-15 16:16:22 +0300

    init

---
+++ MutexThreadsArray/MutexThreadsArray/MutexThreadsArray.cpp
@@ -0,0 +1,118 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <atomic>
+using namespace std;
+
+const int threadsAmount = 4;
+const int arraySize = 1000000;
+
+
+class MutexArray {
+private:
+	
+	int currentIndex = 0;
+	thread threads[threadsAmount];
+	mutex indexLock;
+
+public:
+
+	int* initArray() {
+		int* array = new int[arraySize];
+		for (int i = 0; i < arraySize; i++) {
+			array[i] = 0;
+		}
+		return array;
+	}
+
+	void incrementArray(int id, int* array) {
+		int i = currentIndex;
+		while (true) {
+			indexLock.lock();
+			if (currentIndex >= arraySize)
+				break;
+			i = currentIndex;
+			currentIndex++;
+			indexLock.unlock();
+			array[i]++;
+		}
+	}
+
+	void run() {
+		
+		int* array = initArray();
+
+		for (int i = 0; i < threadsAmount; i++) {
+			threads[i] = thread(&MutexArray::incrementArray, this, i, array);
+		}
+
+		for (int i = 0; i < threadsAmount; i++) {
+			threads[i].join();
+		}
+
+		bool normal = true;
+		for (int i = 0; i < arraySize; i++) {
+			if (array[i] != 1)
+				normal = false;
+		}
+
+		if (normal) cout << "ok";
+	}
+};
+
+class AtomicArray {
+private:
+	atomic<int> index = 0;
+	thread threads[threadsAmount];
+
+public:
+
+	int* initArray() {
+		int* array = new int[arraySize];
+		for (int i = 0; i < arraySize; i++) {
+			array[i] = 0;
+		}
+		return array;
+	}
+
+	void incrementArray(int id, int* array) {
+		int i = index.load();
+		while (i < arraySize) {
+			 i = index.fetch_add(1);
+			 if (i < arraySize) {
+				 array[i]++;
+			 }
+		}
+	}
+
+	void run() {
+
+		int* array = initArray();
+
+		for (int i = 0; i < threadsAmount; i++) {
+			threads[i] = thread(&AtomicArray::incrementArray, this, i, array);
+		}
+
+		for (int i = 0; i < threadsAmount; i++) {
+			threads[i].join();
+		}
+
+		this_thread::sleep_for(chrono::milliseconds(1));
+		bool normal = true;
+		for (int i = 0; i < arraySize; i++) {
+			//cout << array[i];
+			if (array[i] != 1)
+				normal = false;
+		}
+
+		if (normal) cout << "ok";
+	}
+};
+
+
+int main()
+{
+	AtomicArray mut;
+	mut.run();
+}
---
+++ OpenMP/OpenMP/OpenMP.cpp
@@ -0,0 +1,59 @@
+﻿#include "pch.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <ctime>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 1000
+
+int A[N][N];
+int B[N][N];
+int C[N][N];
+
+int main()
+{
+	int i, j, k;
+	double elapsed, elapsed1;
+	omp_set_num_threads(omp_get_num_procs());
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++)
+		{
+			A[i][j] = rand() % 10 + 1;
+			B[i][j] = rand() % 10 + 1;
+		}
+	}
+	clock_t begin1 = clock();
+	for (i = 0; i < N; ++i) {
+		for (j = 0; j < N; ++j) {
+			for (k = 0; k < N; ++k) {
+				C[i][j] += A[i][k] * B[k][j];
+			}
+		}
+	}
+	clock_t end1 = clock();
+	elapsed1 = double(end1 - begin1) / 1000;
+	printf("elapsed time = %f seconds without threads\n ", elapsed1);
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			C[i][j] = 0;
+		}
+	}
+
+	clock_t begin = clock();
+	#pragma omp parallel for private(i,j,k) shared(A,B,C)
+	for (i = 0; i < N; ++i) {
+		for (j = 0; j < N; ++j) {
+			for (k = 0; k < N; ++k) {
+				C[i][j] += A[i][k] * B[k][j];
+			}
+		}
+	}
+
+	clock_t end = clock();
+	elapsed = double(end - begin) / 1000;
+	printf("elapsed time = %f seconds with threads\n ", elapsed);
+	printf("%f", elapsed1 / elapsed);
+}
